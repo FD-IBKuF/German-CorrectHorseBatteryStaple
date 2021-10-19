bbc359db3e02a87e95762a9ad857f193cbe8389f
Felix Dreier, IB-KuF <fd@ib-kuf.de>
2021-10-19 13:06:11 +0200

    Allow custom number of words Closes #1 Closes #2 Closes #3

--- Get-RandomPassword.ps1
+++ Get-RandomPassword.ps1
@@ -7,17 +7,40 @@ param (
     [string]
     $OutputPath = ".\Output.txt",
     [switch]
-    $OutFile
+    $OutFile,
+    [ValidateNotNullOrEmpty()]
+    [Int16]
+    $NumberOfWords = 4,
+    [char]
+    $Separator = "-",
+    [switch]
+    $AddNumber,
+    [switch]
+    $Force
 )
+
+if ($NumberOfWords -lt 4 -and -not $Force) {
+    Write-Error "Number of words can't be below 4. Setting Number to minimum." -Category InvalidData
+    $NumberOfWords = 4
+}
 
 $NumberOfRuns = 0
 
 do {
-    $Words = (Invoke-RestMethod -Uri http://api.corpora.uni-leipzig.de/ws/words/deu_news_2012_1M/randomword/?limit=4).word
-    # $Words = (Invoke-RestMethod -Uri http://api.corpora.uni-leipzig.de/ws/words/deu_wikipedia_2010_1M/randomword/?limit=4).word
-    
-    $RawString = $Words[0] + "-" + $Words[1] + "-" + $Words[2] + "-" + $Words[3]
-    $FilteredString = $RawString -replace " ", "_" -replace "\.", ""
+    $RandomWords = (Invoke-RestMethod -Uri "http://api.corpora.uni-leipzig.de/ws/words/deu_news_2012_1M/randomword/?limit=$Number").word
+    # $RandomWords = (Invoke-RestMethod -Uri http://api.corpora.uni-leipzig.de/ws/words/deu_wikipedia_2010_1M/randomword/?limit=4).word
+
+    # $RawString = $RandomWords[0] + "-" + $RandomWords[1] + "-" + $RandomWords[2] + "-" + $RandomWords[3]
+    $RawString = ""
+    foreach ($Word in $RandomWords) {
+        $RawString += "$Word$Separator"
+    }
+
+    if ($AddNumber -and -not ($RawString -match "[0-9]")) {
+        $RawString += Get-Random -Minimum 0 -Maximum 9
+    }
+
+    $FilteredString = $RawString.TrimEnd("$Separator") -replace " ", "_" -replace "\.", ""
     $FilteredString += "`n"
     $PasswordOutput += $FilteredString
     $NumberOfRuns++
